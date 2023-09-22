4303118296b70011b7123979c29e08c33d0c3d70
Anana99 <ananaitohoinyene99@gmail.com>
2023-09-21 20:42:52 -0700

    update

---
+++ builtin.c
@@ -0,0 +1,91 @@
+#include "terminal.h"
+/**
+ * _myexit - exits the terminal
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if data.argv[0] != "exit"
+ */
+int _myexit(data_t *data)
+{
+	int exitcheck;
+	if (data->argv[1])  /* If there is an exit arguement */
+	{
+		exitcheck = _erratoi(data->argv[1]);
+		if (exitcheck == -1)
+		{
+			data->status = 2;
+			print_error(data, "Illegal number: ");
+			_eputs(data->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		data->err_num = _erratoi(data->argv[1]);
+		return (-2);
+	}
+	data->err_num = -1;
+	return (-2);
+}
+/**
+ * _mycd - changes the current directory of the process
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _mycd(data_t *data)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!data->argv[1])
+	{
+		dir = _getenv(data, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(data, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(data->argv[1], "-") == 0)
+	{
+		if (!_getenv(data, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(data, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(data, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(data->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(data, "can't cd to ");
+		_eputs(data->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(data, "OLDPWD", _getenv(data, "PWD="));
+		_setenv(data, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+/**
+ * _myhelp - changes the current directory of the process
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myhelp(data_t *data)
+{
+	char **arg_array;
+	arg_array = data->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
---
+++ environ.c
@@ -0,0 +1,83 @@
+#include "terminal.h"
+/**
+ * _myenv - prints the current environment
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(data_t *data)
+{
+	print_list_str(data->env);
+	return (0);
+}
+/**
+ * _getenv - gets the value of an environ variable
+ * @data: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(data_t *data, const char *name)
+{
+	list_t *node = data->env;
+	char *p;
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @data: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(data_t *data)
+{
+	if (data->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(data, data->argv[1], data->argv[2]))
+		return (0);
+	return (1);
+}
+/**
+ * _myunsetenv - Remove an environment variable
+ * @data: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenv(data_t *data)
+{
+	int i;
+	if (data->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= data->argc; i++)
+		_unsetenv(data, data->argv[i]);
+	return (0);
+}
+/**
+ * populate_env_list - populates env linked list
+ * @data: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(data_t *data)
+{
+	list_t *node = NULL;
+	size_t i;
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	data->env = node;
+	return (0);
+}
